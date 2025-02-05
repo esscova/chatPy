544d109cd789aadb5938bf808caa100ebe7ade96
wellington moreira <wsantos08@hotmail.com>
2025-02-05 14:04:44 -0300

    tratamento de erros e aumento de ping

--- client.py
+++ client.py
@@ -12,13 +12,16 @@ async def receber_mensagens(websocket):
 
 async def chat_cliente():
     async with websockets.connect("ws://localhost:8765") as websocket:
-        nome = input("Digite seu nome: ")
-        await websocket.send(nome)
-        print("Conectado ao servidor de chat!")
+        try:
+            nome = input("Digite seu nome: ")
+            await websocket.send(nome)
+            print("Conectado ao servidor de chat!")
 
-        await asyncio.gather(
-            enviar_mensagens(websocket),
-            receber_mensagens(websocket)
-        )
+            await asyncio.gather(
+                enviar_mensagens(websocket),
+                receber_mensagens(websocket)
+            )
+        except websockets.exceptions.ConnectionClosedError:
+            print("Conexão com o servidor perdida.")
 
 asyncio.run(chat_cliente())
--- server.py
+++ server.py
@@ -14,13 +14,21 @@ async def chat(websocket):
             for conexao in conexoes:
                 if conexao != websocket:
                     await conexao.send(f"{nome} diz: {mensagem}")
+    except websockets.exceptions.ConnectionClosedError:
+        print(f"{nome} desconectou.")
     finally:
         conexoes.remove(websocket)
         print(f"{nome} desconectou. Total de clientes: {len(conexoes)}")
 
 async def main():
-    async with websockets.serve(chat, "localhost", 8765):
-        print("Servidor de chat WebSocket iniciado em ws://localhost:8765")
-        await asyncio.Future()  
+    async with websockets.serve(
+        chat, 
+        "localhost",
+        8765,
+        ping_interval=60,
+        ping_timeout=30
+        ):
+            print("Servidor de chat WebSocket iniciado em ws://localhost:8765")
+            await asyncio.Future()  
 
 asyncio.run(main())
